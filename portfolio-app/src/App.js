7c926943b388619bcb2a6b5ad5f0db6d47feb579
bibekbajagain8 <101430528+bibekbajagain8@users.noreply.github.com>
2022-12-29 10:43:09 -0600

    change

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,22 +1,22 @@
 import './index.css';
-import styles from "./style";
-import { Navbar } from "./components";
+import styles from "./styles";
+import { Navbar,Hero } from "./components";
 
 const App = () => (
-  <div className="bg-black-gradient w-full overflow-hidden">
+  <div className="bg-green-200 w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`bg-black-gradient ${styles.flexStart}`}>
+    <div className={`bg-green-200 ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-       
+       <Hero />
       </div>
     </div>
     
-    <div className={`bg-black-gradient ${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`bg-green-200 ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         
       </div>
--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -25,10 +25,30 @@ function Navbar() {
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img src={toggle ? close : menu} 
-            alt="menu"
-            className="w-6 h-6 object-contain"
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-6 h-6 object-contain"
+          onClick={() => setToggle((prev) => !prev)}
         />
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`text-white font-poppins font-normal cursor-pointer text-[16px]  ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                }`}
+              >
+                {nav.title}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
