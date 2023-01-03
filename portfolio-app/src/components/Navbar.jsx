badfb1a416b9fc08ee240e013fb5a5e2b2c19b12
bibekbajagain8 <101430528+bibekbajagain8@users.noreply.github.com>
2023-01-02 18:53:17 -0600

    mediaquery for javascript fixed

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -6,50 +6,34 @@ import { BsFillBriefcaseFill } from "react-icons/bs";
 import "../styles/navbar.css";
 
 function Navbar() {
-  const [toggle, setToggle] = useState(false);
+  
+  
 
-  return (
-    <nav className="nav">
-      <BsFillBriefcaseFill />
-      <ul className="ul">
-        {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={` ${index === navLinks.length - 1 ? 'mr.p' : 'mr.q'}`}
-          >
-            {nav.title}
-          </li>
-        ))}
-      </ul>
+  // Create a condition that targets viewports at least 768px wide
+  const mediaQuery = window.matchMedia("(min-width: 768px)");
 
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className="w-6 h-6 object-contain"
-          onClick={() => setToggle((prev) => !prev)}
-        />
-        <div
-          className={`${
-            !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-        >
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={` font-normal cursor-pointer text-[16px]  ${
-                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
-                }`}
-              >
-                {nav.title}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+  function handleTabletChange(e) {
+    // Check if the media query is true
+    if (e.matches) {
+      // Then log the following message to the console
+      console.log('big devices');
+    } else {
+      console.log('small devices');
+    }
+  }
+
+  // Register event listener
+  mediaQuery.addListener(handleTabletChange);
+
+  // Initial check
+  handleTabletChange(mediaQuery);
+
+  return(
+    <>
+      <p>Navbar</p>
+      <button>Click</button>
+    </>
+  )
 }
 
 export default Navbar;
